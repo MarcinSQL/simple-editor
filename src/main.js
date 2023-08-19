5c649e8bcd08463d2dab30156451de6a163da236
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-08-19 20:49:34 +0200

    Added listeners to buttons

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-const textArea = document.querySelector('.editor--js');
-const btnLoad = document.querySelector('.btn-load--js');
-const btnSave = document.querySelector('.btn-save--js');
+const textArea = document.querySelector("#editor--js");
+const btnLoad = document.querySelector("#btn-load--js");
+const btnSave = document.querySelector("#btn-save--js");
+
+btnLoad.addEventListener("click", () => {
+  const result = localStorage.getItem("text");
+  textArea.value = result;
+});
+
+btnSave.addEventListener("click", () => {
+  localStorage.setItem("text", textArea.value);
+});
